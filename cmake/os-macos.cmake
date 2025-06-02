91f639227b215ff442589312a5ced5088fffd87c
Richard <rosborne255@yahoo.com>
2025-06-02 08:17:19 -0500

    Disable ENABLE_BROWSER_QT_LOOP (#40)
    * rename obs64 helper -> OBS helper
    
    * fix browser source configuration for macos
    
    * This will break SLOBS/Desktop if you run it naked BUT if you build it as a package
    browser sources WILL work only there because CEF can find the obs64 helper in Contents/Frameworks

--- cmake/os-macos.cmake
+++ cmake/os-macos.cmake
@@ -1,6 +1,9 @@
-find_package(Qt6 REQUIRED Widgets)
+# We currently do not use Qt in obs-studio-node so its' best to leave this off. OBS.app is a QtApplication so this makes sense for them.
+#find_package(Qt6 REQUIRED Widgets)
 
-target_compile_definitions(obs-browser PRIVATE ENABLE_BROWSER_SHARED_TEXTURE ENABLE_BROWSER_QT_LOOP)
+# Do not turn on ENABLE_BROWSER_QT_LOOP. Once again, obs-studio-node is not a Qt app. We could make it one kinda easily as long as the exact same
+# binaries we use to build obs-browser is packed up and shipped along like libobs.framework. But for now we dont need the ENABLE_BROWSER_QT_LOOP flag
+target_compile_definitions(obs-browser PRIVATE ENABLE_BROWSER_SHARED_TEXTURE)
 
 if(CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL 14.0.3)
   target_compile_options(obs-browser PRIVATE -Wno-error=unqualified-std-cast-call)
@@ -10,7 +13,7 @@ target_link_libraries(obs-browser PRIVATE Qt::Widgets CEF::Wrapper "$<LINK_LIBRA
                                           "$<LINK_LIBRARY:FRAMEWORK,AppKit.framework>")
 
 set(helper_basename browser-helper)
-set(helper_output_name "obs64 Helper")
+set(helper_output_name "obs64 Helper") # See cef_types.h documentation for browser_subprocess_path setting
 set(helper_suffixes "::" " (GPU):_gpu:.gpu" " (Plugin):_plugin:.plugin" " (Renderer):_renderer:.renderer")
 
 foreach(helper IN LISTS helper_suffixes)
