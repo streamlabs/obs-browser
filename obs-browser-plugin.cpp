4890d9aec5222f9202fcb8730d9bdb63a4373e9c
Richard Osborne <rosborne255@yahoo.com>
2025-08-19 19:20:30 -0500

    Revert "remove underscore to fix unused var xcodebuild error (#46)"
    This reverts commit 40e42ac6b6eacba6c4aa1f9ce940c44cac8df169.

--- obs-browser-plugin.cpp
+++ obs-browser-plugin.cpp
@@ -506,7 +506,7 @@ static void BrowserShutdown(void)
 
 #ifndef ENABLE_BROWSER_QT_LOOP
 #if defined(__APPLE__)
-static void BrowserManagerThread(obs_data_t *)
+static void BrowserManagerThread(obs_data_t *_)
 {
 	CefRunMessageLoop();
 	BrowserShutdown();
